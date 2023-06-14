849b1679dfa1dd688dcf10c6704a3e2d5967d166
Christopher Hugentobler <hugentoblerc@gmail.com>
2023-06-14 13:28:11 -0700

    Added shadcn/ui

---
+++ app/demo/page.tsx
@@ -0,0 +1,13 @@
+import PageHeading from "@/components/page-heading"
+import DescriptionList from "@/components/description-list"
+
+export default function Page() {
+  return (
+    <>
+      <PageHeading heading="VHIS Flexi Plus" />
+      <PageHeading heading="Cancer Fighter" />
+      <PageHeading heading="Term Life Insurance" />
+      <DescriptionList />
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import PageHeading from "@/components/page-heading"
-import DescriptionList from "@/components/description-list"
-
 export default function Page() {
   return (
-    <>
-      <PageHeading heading="VHIS Flexi Plus" />
-      <PageHeading heading="Cancer Fighter" />
-      <PageHeading heading="Term Life Insurance" />
-      <DescriptionList />
-    </>
+    <></>
   )
 }
